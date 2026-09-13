28aa1b28e8da75da114f9612c598b3f6d65c0c74
crosseyedCOBRA <crosseyedCOBRA@gmail.com>
2026-09-13 09:36:42 -0400

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -5,27 +5,14 @@
     ./hardware-configuration.nix
   ];
 
-  # ------------------------------------------------------------
-  # Boot
-  # ------------------------------------------------------------
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # ------------------------------------------------------------
-  # Networking
-  # ------------------------------------------------------------
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # ------------------------------------------------------------
-  # X11 / LightDM / dwm
-  # ------------------------------------------------------------
-
   services.xserver = {
     enable = true;
-
     xkb.layout = "us";
 
     displayManager.lightdm = {
@@ -33,47 +20,22 @@
       greeters.gtk.enable = true;
     };
 
-    windowManager.dwm = {
-      enable = true;
-
-      package = pkgs.dwm.overrideAttrs (oldAttrs: {
-        postPatch = (oldAttrs.postPatch or "") + ''
-          cp ${./dwm-config.h} config.h
-        '';
-      });
-    };
+    windowManager.dwm.enable = true;
   };
 
-  # ------------------------------------------------------------
-  # Graphics
-  # ------------------------------------------------------------
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  # ------------------------------------------------------------
-  # Audio
-  # ------------------------------------------------------------
+  hardware.graphics.enable = true;
 
   services.pipewire = {
     enable = true;
     pulse.enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
   };
 
   security.rtkit.enable = true;
 
-  # ------------------------------------------------------------
-  # User
-  # ------------------------------------------------------------
-
   users.users.mike = {
     isNormalUser = true;
     description = "Mike";
-
     extraGroups = [
       "wheel"
       "networkmanager"
@@ -82,86 +44,22 @@
     ];
   };
 
-  # ------------------------------------------------------------
-  # Flatpak + XDG Desktop Portal
-  # ------------------------------------------------------------
-
-  services.flatpak.enable = true;
-
-  xdg.portal = {
-    enable = true;
-
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-
-    config.common.default = [
-      "gtk"
-    ];
-  };
-
-  # ------------------------------------------------------------
-  # Desktop / Applications
-  # ------------------------------------------------------------
-
   environment.systemPackages = with pkgs; [
-    # Terminal
     alacritty
-
-    # Application launcher
     rofi
-
-    # Notifications
-    dunst
-    libnotify
-
-    # XDG integration
-    xdg-utils
-
-    # Audio
-    pavucontrol
-
-    # Development
     git
-
-    # Basic utilities
     vim
     wget
     curl
-    unzip
-    zip
-
-    # System information
-    htop
-    tree
-    pciutils
-    usbutils
-
-    # X11 utilities
-    xorg.xev
-    xorg.xkill
     xorg.xrandr
-    xorg.xdpyinfo
-    xorg.xprop
+    xorg.xev
   ];
 
-  # ------------------------------------------------------------
-  # Services
-  # ------------------------------------------------------------
-
   services.libinput.enable = true;
-
-  # ------------------------------------------------------------
-  # Nix
-  # ------------------------------------------------------------
 
   nix.settings.experimental-features = [
     "nix-command"
   ];
-
-  # ------------------------------------------------------------
-  # System State Version
-  # ------------------------------------------------------------
 
   system.stateVersion = "26.05";
 }
