e9d9b213c30b71dde884c48a5ddc74dbc642d916
crosseyedCOBRA <crosseyedCOBRA@gmail.com>
2026-09-13 08:48:12 -0400

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -5,210 +5,6 @@
     ./hardware-configuration.nix
   ];
 
-  # ---------------------------------------------------------
-  # Boot
-  # ---------------------------------------------------------
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.loader.systemd-boot.configurationLimit = 10;
-
-  # ---------------------------------------------------------
-  # Networking
-  # ---------------------------------------------------------
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  # ---------------------------------------------------------
-  # Locale / Time
-  # ---------------------------------------------------------
-
-  time.timeZone = "America/New_York";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # ---------------------------------------------------------
-  # X11 + LightDM + bspwm
-  # ---------------------------------------------------------
-
-  services.xserver = {
-    enable = true;
-
-    displayManager.lightdm = {
-      enable = true;
-
-      greeters.gtk.enable = true;
-    };
-
-    windowManager.bspwm.enable = true;
-  };
-
-  services.displayManager.defaultSession = "bspwm";
-
-  # ---------------------------------------------------------
-  # Audio - PipeWire
-  # ---------------------------------------------------------
-
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-
-    alsa.enable = true;
-    alsa.support32Bit = true;
-
-    pulse.enable = true;
-  };
-
-  # ---------------------------------------------------------
-  # Graphics
-  # ---------------------------------------------------------
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  # ---------------------------------------------------------
-  # Flatpak
-  # ---------------------------------------------------------
-
-  services.flatpak.enable = true;
-
-  xdg.portal = {
-    enable = true;
-
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
-  };
-
-  # ---------------------------------------------------------
-  # Steam
-  # ---------------------------------------------------------
-
-  nixpkgs.config.allowUnfree = true;
-
-  programs.steam = {
-    enable = true;
-  };
-
-  # ---------------------------------------------------------
-  # Bluetooth
-  # ---------------------------------------------------------
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  # ---------------------------------------------------------
-  # User
-  # ---------------------------------------------------------
-
-  users.users.mike = {
-    isNormalUser = true;
-
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "audio"
-      "video"
-    ];
-  };
-
-  security.sudo.wheelNeedsPassword = true;
-
-  # ---------------------------------------------------------
-  # Basic Software
-  # ---------------------------------------------------------
-
-  environment.systemPackages = with pkgs; [
-    # System / terminal
-    git
-    wget
-    curl
-    unzip
-    zip
-    p7zip
-
-    # Editors
-    vim
-    nano
-
-    # System monitoring
-    btop
-    fastfetch
-
-    # X11 utilities
-    xorg.xrandr
-    xorg.xev
-    xorg.xprop
-    xorg.xdpyinfo
-    xclip
-
-    # Desktop utilities
-    rofi
-    dunst
-    picom
-    feh
-    thunar
-    pavucontrol
-    flameshot
-
-    # Bluetooth
-    bluez
-    blueman
-
-    # Development basics
-    gcc
-    gnumake
-    cmake
-    pkg-config
-  ];
-
-  # ---------------------------------------------------------
-  # Fonts
-  # ---------------------------------------------------------
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.fira-code
-    noto-fonts
-    noto-fonts-emoji
-  ];
-
-  # ---------------------------------------------------------
-  # Environment
-  # ---------------------------------------------------------
-
-  environment.variables = {
-    EDITOR = "nano";
-  };
-
-  # ---------------------------------------------------------
-  # Nix
-  # ---------------------------------------------------------
-
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-    ];
-
-    auto-optimise-store = true;
-  };
-
-  # ---------------------------------------------------------
-  # System Version
-  # ---------------------------------------------------------
-
-  system.stateVersion = "26.05";
-}{ config, pkgs, ... }:
-
-{
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
   # ------------------------------------------------------------
   # Boot
   # ------------------------------------------------------------
@@ -222,14 +18,6 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
-  # ------------------------------------------------------------
-  # Locale / Time
-  # ------------------------------------------------------------
-
-  time.timeZone = "America/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
 
   # ------------------------------------------------------------
   # X11
@@ -238,62 +26,35 @@
   services.xserver = {
     enable = true;
 
-    # LightDM
+    xkb.layout = "us";
+
     displayManager.lightdm = {
       enable = true;
       greeters.gtk.enable = true;
     };
 
-    # Qtile
-    windowManager.qtile = {
-      enable = true;
-
-      extraPackages = python3Packages: with python3Packages; [
-        qtile-extras
-      ];
-    };
-  };
-
-  # Explicitly use the X11 Qtile session
-  services.displayManager.defaultSession = "qtile";
-
-  # ------------------------------------------------------------
-  # Audio
-  # ------------------------------------------------------------
-
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-
-    alsa.enable = true;
-    alsa.support32Bit = true;
-
-    pulse.enable = true;
-  };
-
-  # ------------------------------------------------------------
-  # Flatpak
-  # ------------------------------------------------------------
-
-  services.flatpak.enable = true;
-
-  # ------------------------------------------------------------
-  # Steam
-  # ------------------------------------------------------------
-
-  programs.steam = {
-    enable = true;
+    windowManager.dwm.enable = true;
   };
 
   # ------------------------------------------------------------
   # Graphics
   # ------------------------------------------------------------
 
-  hardware.graphics = {
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+
+  # ------------------------------------------------------------
+  # Audio
+  # ------------------------------------------------------------
+
+  services.pipewire = {
     enable = true;
-    enable32Bit = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
   };
+
+  security.rtkit.enable = true;
 
   # ------------------------------------------------------------
   # User
@@ -301,128 +62,89 @@
 
   users.users.mike = {
     isNormalUser = true;
+    description = "Mike";
 
     extraGroups = [
       "wheel"
       "networkmanager"
-      "audio"
       "video"
+      "audio"
     ];
   };
 
-  # Allow sudo for mike
-  security.sudo.wheelNeedsPassword = true;
+  # ------------------------------------------------------------
+  # Flatpak + XDG Desktop Portal
+  # ------------------------------------------------------------
+
+  services.flatpak.enable = true;
+
+  xdg.portal = {
+    enable = true;
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+
+    config.common.default = [
+      "gtk"
+    ];
+  };
 
   # ------------------------------------------------------------
-  # Packages
+  # Desktop / Applications
   # ------------------------------------------------------------
 
   environment.systemPackages = with pkgs; [
+    # Terminal
+    alacritty
+
+    # Application launcher
+    rofi
+
+    # Notifications
+    dunst
+    libnotify
+
+    # XDG integration
+    xdg-utils
+
+    # Audio control
+    pavucontrol
+
+    # Development
+    git
 
     # Basic utilities
-    git
+    vim
     wget
     curl
     unzip
     zip
 
-    # Terminal/editor
-    vim
-    nano
+    # System information
+    htop
+    tree
+    pciutils
+    usbutils
 
-    # System information / monitoring
-    btop
-    fastfetch
-
-    # X11 utilities
-    xorg.xrandr
+    # X11 troubleshooting / utilities
     xorg.xev
-    xorg.xprop
+    xorg.xkill
+    xorg.xrandr
     xorg.xdpyinfo
-    xclip
-
-    # Qtile helpers
-    rofi
-    dunst
-    picom
-    feh
-
-    # File management
-    thunar
-
-    # Audio
-    pavucontrol
-
-    # Bluetooth
-    bluez
-    blueman
-
-    # Screenshot
-    flameshot
-
-    # Archive utilities
-    p7zip
-
-    # Build/development tools
-    gcc
-    gnumake
-    cmake
-    pkg-config
-
-    # ZarisWM development dependencies
-    glib
-    libxcb
-    xcb-util
-    xcb-util-cursor
-    xcb-util-keysyms
-    xcb-util-wm
-    xcb-util-image
-    xcb-util-renderutil
+    xorg.xprop
   ];
 
   # ------------------------------------------------------------
-  # Bluetooth
+  # Nix
   # ------------------------------------------------------------
 
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  # ------------------------------------------------------------
-  # Fonts
-  # ------------------------------------------------------------
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.fira-code
-    noto-fonts
-    noto-fonts-emoji
+  nix.settings.experimental-features = [
+    "nix-command"
   ];
 
   # ------------------------------------------------------------
-  # Environment
-  # ------------------------------------------------------------
-
-  environment.variables = {
-    EDITOR = "nano";
-  };
-
-  # ------------------------------------------------------------
-  # Nix settings
-  # ------------------------------------------------------------
-
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-    ];
-
-    auto-optimise-store = true;
-  };
-
-  # Keep old generations around so we can roll back if needed.
-  boot.loader.systemd-boot.configurationLimit = 10;
-
-  # ------------------------------------------------------------
-  # State version
+  # System State Version
   # ------------------------------------------------------------
 
   system.stateVersion = "26.05";
